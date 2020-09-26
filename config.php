3f1a0ad2c6c4415859c73e1477266704809eec55
lang999 <39425761+lang999@users.noreply.github.com>
2020-09-26 21:23:04 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,4 +2,4 @@
 //$proxy="http://192.168.0.1:1282";//代理服务器地址 支持http,socks4,socks5 如果没有请留空
 // eg:// http://192.168.0.1:80;socket5://192.168.0.5:4455;这样的，记得要带端口号。代理地址只填一个，没做多个检测随机抽取功能。
 $key='hello_world';//aes密码
-define('COOKIE',"__cfduid=d6005dc5aaa20a4869ae7e9525bc58b721600431874; __utmz=202858260.1600774580.2.2.utmcsr=91p52.com|utmccn=(referral)|utmcmd=referral|utmcct=/; CLIPSHARE=dj1pbgd3muu21gmce91q63jsv4; __utma=202858260.1532773168.1600431885.1600783385.1601123890.4; __utmb=202858260.0.10.1601123890; __utmc=202858260");
+define('COOKIE',"__cfduid=d4f42f90f024773e2078aac11cd1d61d21598784448; _ga=GA1.2.2049714014.1598784474; _gid=GA1.2.782623385.1601125312");
